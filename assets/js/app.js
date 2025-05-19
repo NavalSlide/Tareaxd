01562483f4aad053ab4b0d4ada09c57ea5b0b34b
Mortis <mortisclips999@gmail.com>
2025-05-19 07:51:38 -0500

    bubles agregados

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,3 +44,36 @@ document.addEventListener("DOMContentLoaded", function() {
     
     ResaltarPaginaActual(currentPage);
 });
+
+function CrearBurbujas() {
+    const bubbleContainer = document.querySelector('.bubble-container');
+    const ContadorBurbuja = 10; 
+
+    for (let i = 0; i < ContadorBurbuja; i++) {
+        const bubble = document.createElement('div');
+        bubble.classList.add('bubble');
+
+        
+        const size = Math.random() * 60 + 40; 
+        const left = Math.random() * 100; 
+        const duration = Math.random() * 10 + 5; 
+        const sway = (Math.random() - 0.5) * 2; 
+
+        bubble.style.width = `${size}px`;
+        bubble.style.height = `${size}px`;
+        bubble.style.left = `${left}%`;
+        bubble.style.animationDuration = `${duration}s`;
+        bubble.style.setProperty('--sway', sway);
+        bubble.style.animationDelay = `${Math.random() * 5}s`;
+
+        bubbleContainer.appendChild(bubble);
+
+        bubble.addEventListener('animationend', () => {
+            bubble.remove();
+        });
+    }
+}
+
+CrearBurbujas();
+
+setInterval(createBubbles, 7000);
